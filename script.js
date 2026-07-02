dd4e2d8eccca50f099653acfc31a9c94e135dc55
Tripti Singh <71418879+Trptisingh@users.noreply.github.com>
2026-07-02 13:52:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 let messages = [];
 let currentPerson = null;
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx1o25y1NVec6GE3bmYSYg7N992Jh2ZogOL3IRguqp7TyaU5i4HsqPweszuTQ1XKKbI/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwCzmHLqbMSzbMLGeJ9bF87-cQ4HIT5sJ-TCCN07rUDOHd0FlYavCgPJrZtQm0KZQoQ/exec";
 
 /* ===========================================================
    DOM ELEMENTS
